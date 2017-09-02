388688adebd80c3ba57fa5bd637eb138339d3f16
Gesh4o <georgi.stoimenov_96@abv.bg>
2017-09-02 16:49:33 +0300

    Update responsiveness on home page.

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from '../post-module/post/post.component';
 
 const routes: Routes = [
-  { path: '', component: PostComponent }
+  { path: '', component: PostComponent },
+  { path: 'about', component: PostComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
