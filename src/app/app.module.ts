7e6a53f87da5e4e7b0fe313ca51f34d07de99999
Gesh4o <georgi.stoimenov_96@abv.bg>
2017-09-02 12:16:47 +0300

    Initial project structure implementation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared-module/shared.module';
+import { PostModule } from './post-module/post.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PostModule,
+    SharedModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
