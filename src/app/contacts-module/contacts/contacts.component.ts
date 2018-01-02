a5360d1544088b654246c0732acd0ebf2de52be9
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-01-02 10:33:02 +0200

    Updates animations on contact's page.

--- src/app/contacts-module/contacts/contacts.component.ts
+++ src/app/contacts-module/contacts/contacts.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
+  public shouldBeVisible = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public response () {
+    this.shouldBeVisible = !this.shouldBeVisible;
+  }
 }
