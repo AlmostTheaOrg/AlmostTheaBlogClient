5a84116dd02d0c9d898c99969d007e72d0e2db92
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-01-03 15:57:19 +0200

    Adds Enlarging image (portraits) on click.

--- src/app/portraits-module/portraits.module.ts
+++ src/app/portraits-module/portraits.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PortraitsComponent } from './portraits/portraits.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { PortraitsComponent } from "./portraits/portraits.component";
 
 @NgModule({
   imports: [
@@ -8,4 +8,4 @@ import { PortraitsComponent } from './portraits/portraits.component';
   ],
   declarations: [PortraitsComponent]
 })
-export class PortraitsModule { }
+export class PortraitsModule {}
--- src/app/portraits-module/portraits/portraits.component.ts
+++ src/app/portraits-module/portraits/portraits.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-portraits',
-  templateUrl: './portraits.component.html',
-  styleUrls: ['./portraits.component.css']
+  selector: "app-portraits",
+  templateUrl: "./portraits.component.html",
+  styleUrls: ["./portraits.component.css"]
 })
 export class PortraitsComponent implements OnInit {
+  @Output() private classChange = new EventEmitter();
+  private nonSelected = "lightbox-target";
+  private selected = this.nonSelected + " selected";
 
-  constructor() { }
+  private image = { class: "lightbox-target", imageSrc: "" };
 
-  ngOnInit() {
+  private focused = false;
+
+  private portraits = [
+    { imgSrc: "assets/images/portrait-1.jpg", name: "Biktor" },
+    { imgSrc: "assets/images/portrait-2.jpg", name: "Goshy" },
+    { imgSrc: "assets/images/portrait-3.jpg", name: "Elly" },
+    { imgSrc: "assets/images/portrait-1.jpg", name: "Biktor" },
+    { imgSrc: "assets/images/portrait-1.jpg", name: "Biktor" },
+    { imgSrc: "assets/images/portrait-1.jpg", name: "Biktor" },
+    { imgSrc: "http://i.huffpost.com/gen/749263/original.jpg", name: "Doggie" }
+  ];
+
+  ngOnInit() {}
+
+  @Input()
+  get class(): Object {
+    return this.image;
   }
 
+  enlarge(portrait) {
+    this.image.class = this.selected;
+    this.image.imageSrc = portrait.imgSrc;
+
+    this.classChange.emit(this.image);
+  }
+
+  close() {
+    this.image.class = this.nonSelected;
+    this.image.imageSrc = "";
+
+    console.log('close')
+    this.classChange.emit(this.image);
+  }
 }
