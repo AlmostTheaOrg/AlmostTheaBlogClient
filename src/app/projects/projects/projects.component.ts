88811e0139ffdc0499a3b2918d46f94835ef25ca
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-01-14 11:03:25 +0200

    Initial project's page design.

--- src/app/projects/projects/projects.component.ts
+++ src/app/projects/projects/projects.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+	selector: 'app-projects',
+	templateUrl: './projects.component.html',
+	styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+	public projects: Array<{ name: string, pictures: number, thumb: string }> = [
+		{
+			name: 'Nature',
+			pictures: 14,
+			thumb: 'https://images.pexels.com/photos/34950/pexels-photo.jpg?w=940&h=650&auto=compress&cs=tinysrgb'
+		},
+		{
+			name: 'Civil',
+			pictures: 8,
+			thumb: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/ls-sample6.jpg'
+		},
+		{
+			name: 'Animals',
+			pictures: 11,
+			thumb: 'https://images.pexels.com/photos/798575/pexels-photo-798575.jpeg?w=940&h=650&auto=compress&cs=tinysrgb'
+		},
+		{
+			name: 'Coffee',
+			pictures: 3,
+			thumb: 'https://images.pexels.com/photos/796611/pexels-photo-796611.jpeg?w=940&h=650&auto=compress&cs=tinysrgb'
+		},
+		{
+			name: 'Casual',
+			pictures: 5,
+			thumb: 'https://images.pexels.com/photos/773736/pexels-photo-773736.jpeg?w=940&h=650&auto=compress&cs=tinysrgb'
+		}
 
-  constructor() { }
+	];
+	constructor() { }
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+	}
 }
