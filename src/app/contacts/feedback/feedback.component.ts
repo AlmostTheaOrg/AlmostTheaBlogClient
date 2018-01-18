6ccff6e0381a88cb78536d98d3c29e13f58d9932
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-01-18 14:40:49 +0200

    Adds feedback's page design.

--- src/app/contacts/feedback/feedback.component.ts
+++ src/app/contacts/feedback/feedback.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-feedback',
-  templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+	selector: 'app-feedback',
+	templateUrl: './feedback.component.html',
+	styleUrls: ['./feedback.component.css']
 })
-export class FeedbackComponent implements OnInit {
+export class FeedbackComponent {
+	public feedbacks = [
+		{
+			id: 1, name: 'Pesho', email: 'tudor@gmail.com', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+		},
+		{ id: 2, name: 'Gosho', email: 'gosho@gmail.com', content: 'Lorizzle ipsizzle dolizzle pizzle go to hizzle, fo shizzle.' },
+		{ id: 3, name: 'Maria', email: 'maria@gmail.com', content: 'Climb leg rub face on everything give attitude nap all day.' },
+		{ id: 4, name: 'Pesho', email: 'tudor@gmail.com', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+		{ id: 5, name: 'Gosho', email: 'gosho@gmail.com', content: 'Lorizzle ipsizzle dolizzle pizzle go to hizzle, fo shizzle.' },
+		{ id: 6, name: 'Maria', email: 'maria@gmail.com', content: 'Climb leg rub face on everything give attitude nap all day.' },
+		{ id: 7, name: 'Pesho', email: 'tudor@gmail.com', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+		{ id: 8, name: 'Gosho', email: 'gosho@gmail.com', content: 'Lorizzle ipsizzle dolizzle pizzle go to hizzle, fo shizzle.' },
+		{ id: 9, name: 'Maria', email: 'maria@gmail.com', content: 'Climb leg rub face on everything give attitude nap all day.' },
+	];
 
-  constructor() { }
+	private opened;
 
-  ngOnInit() {
-  }
+	private selected = new Set();
 
+	constructor() { }
+
+	select(feedback) {
+		this.opened = feedback;
+	}
+
+	markOrUnmark(feedback) {
+		console.log('click');
+		if (this.selected.has(feedback.id)) {
+			this.selected.delete(feedback.id);
+		} else {
+			this.selected.add(feedback.id);
+		}
+	}
+
+	close() {
+		this.opened = null;
+	}
+
+	delete() {
+		if (this.opened && this.selected.has(this.opened.id)) {
+			this.close();
+		}
+
+		this.feedbacks = this.feedbacks.filter(f => !this.selected.has(f.id));
+		this.selected.clear();
+	}
 }
