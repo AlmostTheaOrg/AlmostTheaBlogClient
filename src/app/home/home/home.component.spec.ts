3b070d20c06c2130d69229cd3c78034c9e3b8d22
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-01-28 19:06:32 +0200

    Removes wrong reference to renamed componend.

--- src/app/home/home/home.component.spec.ts
+++ src/app/home/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PostComponent } from './post.component';
+import { HomeComponent } from './home.component';
 
-describe('PostComponent', () => {
-	let component: PostComponent;
-	let fixture: ComponentFixture<PostComponent>;
+describe('HomeComponent', () => {
+	let component: HomeComponent;
+	let fixture: ComponentFixture<HomeComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ PostComponent ]
+			declarations: [ HomeComponent ]
 		})
 		.compileComponents();
 	}));
 
 	beforeEach(() => {
-		fixture = TestBed.createComponent(PostComponent);
+		fixture = TestBed.createComponent(HomeComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
