42efbea556748bdbc2d7054fa709e50d22805c5f
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-03-18 20:00:12 +0200

    Merges changes with local branch.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,16 @@
 import { HttpRecaptchaValidatorService } from '../app/services/recaptcha-validator.service';
 
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-	production: true,
-	recaptcha_site_key: '6LeGMUQUAAAAAO9WIM_XysIDiJO1V8nkRkFSAUvl',
+	production: false,
+	recaptcha_site_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
 	recaptcha_validator: HttpRecaptchaValidatorService,
-	backend_url: ''
+	server_url: 'http://localhost:3000/api/',
+	crypto : {
+		password: 'dsa213p',
+	}
 };
