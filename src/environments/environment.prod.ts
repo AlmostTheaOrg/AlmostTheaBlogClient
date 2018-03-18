aa70aff1ae3b424c1e1f4bbc4320b748eb802ff5
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-03-18 20:05:20 +0200

    Modifies envs.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,8 @@ export const environment = {
 	production: true,
 	recaptcha_site_key: '6LeGMUQUAAAAAO9WIM_XysIDiJO1V8nkRkFSAUvl',
 	recaptcha_validator: HttpRecaptchaValidatorService,
-	backend_url: ''
+	backend_url: 'https://almost-thea-api.herokuapp.com/api/',
+	crypto : {
+		password: '31nfoa',
+	}
 };
