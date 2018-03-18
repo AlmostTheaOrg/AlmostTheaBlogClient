b1c2257c10638b52483e8c6068bca545e3a7230f
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-03-18 20:09:28 +0200

    Fixes build violations.

--- src/app/services/application-storage.service.ts
+++ src/app/services/application-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import CryptoJS = require('crypto-js');
+import * as CryptoJS from 'crypto-js';
 import { CryptoService } from './crypto.service';
 
 @Injectable()
