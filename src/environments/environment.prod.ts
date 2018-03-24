75771f7675731d7730e136f99e480610e757c163
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-03-24 08:59:26 +0200

    Removes redundant comments and updates production environment to use dummy recaptcha validator.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,9 @@
-import { HttpRecaptchaValidatorService } from '../app/services/recaptcha-validator.service';
-
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+import { DummyRecaptchaValidatorService } from '../app/services/recaptcha-validator.service';
 
 export const environment = {
-	production: false,
+	production: true,
 	recaptcha_site_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
-	recaptcha_validator: HttpRecaptchaValidatorService,
+	recaptcha_validator: DummyRecaptchaValidatorService,
 	server_url: 'http://localhost:3000/api/',
 	crypto : {
 		password: 'dsa213p',
