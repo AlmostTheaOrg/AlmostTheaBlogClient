81046b28571757973c6139430d6a4ab9277d7be5
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-03-24 09:03:45 +0200

    Reverts production environment to the test recaptcha site key.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { DummyRecaptchaValidatorService } from '../app/services/recaptcha-valida
 
 export const environment = {
 	production: true,
-	recaptcha_site_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+	recaptcha_site_key: '6LeGMUQUAAAAAO9WIM_XysIDiJO1V8nkRkFSAUvl',
 	recaptcha_validator: DummyRecaptchaValidatorService,
 	server_url: 'http://localhost:3000/api/',
 	crypto : {
