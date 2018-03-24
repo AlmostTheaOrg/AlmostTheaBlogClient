7f3c05f9d65bf465e271a79f0bc115a0d8829eab
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-03-24 09:07:13 +0200

    Fixes server url.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 	production: true,
 	recaptcha_site_key: '6LeGMUQUAAAAAO9WIM_XysIDiJO1V8nkRkFSAUvl',
 	recaptcha_validator: DummyRecaptchaValidatorService,
-	server_url: 'http://localhost:3000/api/',
+	server_url: 'https://almost-thea-api.herokuapp.com/api/',
 	crypto : {
 		password: '31nfoa',
 	}
