8efd582b68975a2c322a27d4f583c01dddec2e58
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-03-25 15:32:07 +0300

    Merges from cloud.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
 	production: true,
 	recaptcha_site_key: '6LeGMUQUAAAAAO9WIM_XysIDiJO1V8nkRkFSAUvl',
 	recaptcha_validator: DummyRecaptchaValidatorService,
-	server_url: 'https://almost-thea-api.herokuapp.com/api/',
+	server_url: 'http://localhost:3000/api/',
 	crypto : {
-		password: '31nfoa',
+		password: 'dsa213p',
 	}
 };
