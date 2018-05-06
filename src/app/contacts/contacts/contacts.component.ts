84393b57376e65a3f9feb1080cff6fa7b5fd35cb
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-05-06 10:32:58 +0300

    Improves feedback's form validation.

--- src/app/contacts/contacts/contacts.component.ts
+++ src/app/contacts/contacts/contacts.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FeedbackActions } from '../feedback.actions';
 import { Feedback } from '../../services/feedback.service';
 import { Router } from '@angular/router';
 import { RecaptchaValidator } from '../../services/recaptcha-validator.service';
 import { SharedActions } from '../../shared/shared.actions';
+import { Form } from '@angular/forms';
 
 @Component({
 	selector: 'app-contacts',
@@ -12,13 +13,16 @@ import { SharedActions } from '../../shared/shared.actions';
 })
 export class ContactsComponent {
 	feedback: {
-		name: string,
-		email?: string,
-		captcha?: '',
-		content: string
+		name: string;
+		email?: string;
+		captcha?: string;
+		content: string;
 	} = { name: '', content: '' };
 	loading: boolean;
 	isRecaptchaValid: boolean;
+
+	@ViewChild('contactUs')
+	contactUs: HTMLFormElement;
 
 	constructor(private feedbackActions: FeedbackActions,
 		private sharedActions: SharedActions,
@@ -26,21 +30,64 @@ export class ContactsComponent {
 		private recaptchaValidator: RecaptchaValidator) { }
 
 	resolved(response) {
-		this.recaptchaValidator.validate(response)
-			.then((result) => {
-				this.isRecaptchaValid = result;
-			});
+		this.recaptchaValidator.validate(response).then(result => {
+			this.isRecaptchaValid = result;
+		});
+	}
+
+	validateName() {
+		const isNameValid = /^[a-zA-Z]{3,30}$/.test(this.feedback.name);
+		this.markErrorIfAny(isNameValid, 'name');
+	}
+
+	validateEmail() {
+		const isEmailValid = /^(.+)@(.+){2,}\.(.+){2,}$/.test(this.feedback.email);
+		this.markErrorIfAny(isEmailValid, 'email');
+	}
+
+	validateMessage() {
+		const isContentValid = /^[\w!\,\\\/:\.\-\?\s\"]{10,500}$/.test(this.feedback.content);
+		this.markErrorIfAny(isContentValid, 'message');
 	}
 
 	onSubmit() {
 		this.loading = true;
-		this.feedbackActions.add(this.feedback).then(() => {
-			this.loading = false;
-			this.router.navigateByUrl('/');
-			this.sharedActions.showSuccess('Thank you for your feedback!');
-		}).catch(err => {
-			this.loading = false;
-			this.sharedActions.showDanger('There was an error submitting your feedback. Please try again later!');
-		});
+		this.feedbackActions
+			.add(this.feedback)
+			.then(() => {
+				this.loading = false;
+				this.router.navigateByUrl('/');
+				this.sharedActions.showSuccess('Thank you for your feedback!');
+			})
+			.catch(err => {
+				this.loading = false;
+				this.sharedActions.showDanger(
+					'There was an error submitting your feedback. Please try again later!'
+				);
+			});
+	}
+
+	isNameCharacterAllowed(event: KeyboardEvent) {
+		return this.isAlphabet(event.keyCode);
+	}
+
+	isEmailCharacterAllowed(event: KeyboardEvent) {
+		return this.isAlphanumeric(event.keyCode);
+	}
+
+	private isAlphabet(key: number) {
+		return ((key >= 65 && key <= 90) || key === 8);
+	}
+
+	private isAlphanumeric(key: number) {
+		return this.isAlphabet(key) || (key >= 48 && key <= 57);
+	}
+
+	private markErrorIfAny(isValid: boolean, control: string) {
+		if (!isValid) {
+			this.contactUs.form.controls[control].setErrors({ 'incorrect': true });
+		} else {
+			this.contactUs.form.controls[control].setErrors(null);
+		}
 	}
 }
