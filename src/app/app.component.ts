35307040a4947b8ad513f734228aac6414d42517
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-05-11 23:15:31 +0300

    Adds fade animation and updates footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from './shared/animations/fade.animation';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
+	animations: [fadeAnimation]
 })
 export class AppComponent {
+	public getRouterOutletState(outlet) {
+		return outlet.isActivated ? outlet.activatedRoute : '';
+	}
 }
