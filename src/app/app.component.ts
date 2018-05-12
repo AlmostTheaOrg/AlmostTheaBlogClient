232faef42369e52bce22b3afee94e987943f895f
Gesh4o <georgi.stoimenov_96@abv.bg>
2018-05-12 18:26:19 +0300

    Disables right click on main attribute.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { fadeAnimation } from './shared/animations/fade.animation';
+declare var document: Document;
 
 @Component({
 	selector: 'app-root',
@@ -8,7 +9,24 @@ import { fadeAnimation } from './shared/animations/fade.animation';
 	animations: [fadeAnimation]
 })
 export class AppComponent {
+	constructor() {
+		if (document.addEventListener) {
+			const allImages = document.querySelectorAll('img');
+			Array.from(allImages).forEach(item => {
+				console.log(item);
+				item.addEventListener('contextmenu', function (e) {
+					alert('You\'ve tried to open context menu');
+					e.preventDefault();
+				}, false);
+			});
+		}
+	}
+
 	public getRouterOutletState(outlet) {
 		return outlet.isActivated ? outlet.activatedRoute : '';
+	}
+
+	public handleRightClick() {
+		return false;
 	}
 }
